9a386a95180faf242f13d012e6518184bd86e682
Aditya Maurya <2082001aditya@gmail.com>
2023-06-29 10:25:54 +0530

    added some changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signin, signup } from "../controllers/user.js";
-
+console.log("Router / user folder");
 const router = express.Router();
 router.post("/signin", signin);
 router.post("/signup", signup);
